0d1d10dfcb2183f4614abb5ea12b9ed2a37105e1
Weedshaker <weedshaker@gmail.com>
2023-11-26 11:20:45 +0100

    linter

--- es/components/views/Input.js
+++ es/components/views/Input.js
@@ -132,6 +132,7 @@ export default class Input extends Shadow() {
   }
 
   isTouchScreen () {
+    // @ts-ignore
     return ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
   }
 
